c180d30fc5626796a43fbba5f3a436947d1e52bf
Ubuntu <ubuntu@ip-172-31-19-25.ap-south-1.compute.internal>
2016-11-20 19:23:40 +0000

    added live urls

--- nsepy/liveurls.py
+++ nsepy/liveurls.py
@@ -17,6 +17,26 @@ headers = {'Accept' : '*/*',
           'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0',
           'X-Requested-With': 'XMLHttpRequest'
           }
-get_quote = URLFetch(url = 'http://nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp',
+
+"""
+1. Stock symbol
+2. Series eg. EQ, N1 ...
+"""
+quote_eq_url = URLFetch(url='https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxGetQuoteJSON.jsp?symbol=%s&series=%s',
                      headers=headers)
 
+"""
+1. Underlying security (stock symbol or index name)
+2. instrument (FUTSTK, OPTSTK, FUTIDX, OPTIDX)
+3. expiry (ddMMMyyyy)
+4. type (CE/PE for options, - for futures
+5. strike (strike price upto two decimal places
+"""
+quote_derivative_url = URLFetch(url='https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxFOGetQuoteJSON.jsp?underlying=%s&instrument=%s&expiry=%s&type=%s&strike=%s', headers=headers)
+
+"""
+1. Underlying symbol
+2. instrument (FUTSTK, OPTSTK, FUTIDX, OPTIDX)
+3. expiry date (ddMMMyyyy) where dd is not padded with zero when date is single digit
+"""
+option_chain_url = URLFetch(url='https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?segmentLink=17&symbol=%s&instrument=%s&ate=%s')
---
+++ tests/test_liveurls.py
@@ -0,0 +1,45 @@
+from nsepy.commons import (is_index, is_index_derivative,
+                           NSE_INDICES, INDEX_DERIVATIVES,
+                           ParseTables, StrDate, unzip_str,
+                           ThreadReturns, URLFetch)
+import datetime
+import unittest
+from bs4 import BeautifulSoup
+from tests import htmls
+import json
+import requests
+import six
+from nsepy.liveurls import quote_eq_url, quote_derivative_url, option_chain_url
+import nsepy.urls as urls
+import urlparse
+
+class TestLiveUrls(unittest.TestCase):
+    def setUp(self):
+        proxy_on = False
+        if proxy_on:
+            urls.session.proxies.update({'http':'proxy1.wipro.com:8080'})
+
+    def runTest(self):
+        for key in TestUrls.__dict__.keys():
+            if key.find('test') == 0:
+                TestUrls.__dict__[key](self)
+
+    def test_quote_eq_url(self):
+        resp = quote_eq_url('SBIN', 'EQ')
+        d = json.loads(resp.content)
+        self.assertEqual(d['data'][0]['symbol'], 'SBIN')
+
+    def test_quote_derivative_url(self):
+        resp = quote_derivative_url("NIFTY", "FUTIDX", "24NOV2016", '-', '-')
+        d = json.loads(resp.content)
+        self.assertEqual(d['data'][0]['underlying'], 'NIFTY')
+
+    def test_option_chain_url(self):
+        """
+            1. Underlying symbol
+            2. instrument (FUTSTK, OPTSTK, FUTIDX, OPTIDX)
+            3. expiry date (ddMMMyyyy) where dd is not padded with zero when date is single digit
+        """
+
+        resp = option_chain_url('SBIN', 'OPTSTK', '29NOV2016')
+        self.assertGreaterEqual(resp.text.find('Open Interest'), 0)
