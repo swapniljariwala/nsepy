47cf0812083c5be80d9ca06908671d497d8b4d00
fuzzyhandle <fuzzyhandle@github.com>
2019-07-10 17:25:10 +0100

    Add support for Weekly Index Options
    NSE has started Weekly Index Options
    Index futures, Stock futures and
    Stock Options continue to expire on last Thursday of the month
    
    Update requirement.txt with the latest versions

--- tests/test_live.py
+++ tests/test_live.py
@@ -17,6 +17,7 @@ from nsepy.commons import (is_index, is_index_derivative,
                            ThreadReturns, URLFetch)
 from nsepy import get_expiry_date
 
+
 class TestLiveUrls(unittest.TestCase):
     def setUp(self):
         pass
@@ -31,8 +32,7 @@ class TestLiveUrls(unittest.TestCase):
         comp_name = q['companyName']
         self.assertEqual(comp_name, "State Bank of India")
 
-    def test_get_quote_der(self):
-
+    def test_get_quote_stock_der(self):
         """
         1. Underlying security (stock symbol or index name)
         2. instrument (FUTSTK, OPTSTK, FUTIDX, OPTIDX)
@@ -41,19 +41,58 @@ class TestLiveUrls(unittest.TestCase):
         5. strike (strike price upto two decimal places
         """
         n = datetime.datetime.now()
-        exp = get_expiry_date(n.year, n.month)
-        
-        if n.date() > exp:
+        stexp = get_expiry_date(n.year, n.month, index=False, stock=True)
+        self.assertEqual(len(stexp), 1)
+
+        if n.date() > list(stexp)[0]:
             try:
-                exp = get_expiry_date(n.year, n.month + 1)
+                stexp = get_expiry_date(
+                    n.year, n.month + 1, index=False, stock=True)
             except:
-                exp = get_expiry_date(n.year + 1, 1)
-        
-        
+                stexp = get_expiry_date(n.year + 1, 1, index=False, stock=True)
+
+        self.assertEqual(len(stexp), 1)
+        exp = min([x for x in stexp if x > n.date()])
         q = get_quote(symbol='SBIN', instrument='FUTSTK', expiry=exp)
         comp_name = q['instrumentType']
         self.assertEqual(comp_name, "FUTSTK")
 
+        exp = min([x for x in stexp if x > n.date()])
+        q = get_quote(symbol='SBIN', instrument='OPTSTK',
+                      expiry=exp, option_type="CE", strike=300)
+        comp_name = q['instrumentType']
+        self.assertEqual(comp_name, "OPTSTK")
 
+    def test_get_quote_index_der(self):
+        """
+        1. Underlying security (stock symbol or index name)
+        2. instrument (FUTSTK, OPTSTK, FUTIDX, OPTIDX)
+        3. expiry (ddMMMyyyy)
+        4. type (CE/PE for options, - for futures
+        5. strike (strike price upto two decimal places
+        """
+        n = datetime.datetime.now()
+        stexp = get_expiry_date(n.year, n.month)
+        # 4 weekly expiry per month
+        # Corner case where 1 Feb is a holiday and its non-leap year Feb will only get 3 expiries in the month
+        # So test for only >=3
+        self.assertGreaterEqual(len(stexp), 3)
 
+        if n.date() > max(stexp):
+            try:
+                stexp = get_expiry_date(n.year, n.month + 1)
+            except:
+                stexp = get_expiry_date(n.year + 1, 1)
 
+        self.assertGreaterEqual(len(stexp), 3)
+
+        exp = max([x for x in stexp if x > n.date()])
+        q = get_quote(symbol='NIFTY', instrument='FUTIDX', expiry=exp)
+        comp_name = q['instrumentType']
+        self.assertEqual(comp_name, "FUTIDX")
+
+        exp = min([x for x in stexp if x > n.date()])
+        q = get_quote(symbol='NIFTY', instrument='OPTIDX',
+                      expiry=exp, option_type="CE", strike=11000)
+        comp_name = q['instrumentType']
+        self.assertEqual(comp_name, "OPTIDX")
