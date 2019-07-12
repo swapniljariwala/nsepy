c712c18c85b98dcf95c6e510ca309e545c2b16a7
fuzzyhandle <fuzzyhandle@github.com>
2019-07-12 03:51:26 +0100

    Add function to read and parse the options chain table from nse

--- nsepy/live.py
+++ nsepy/live.py
@@ -7,12 +7,21 @@ Created on Fri Dec 18 21:51:41 2015
 from nsepy.commons import *
 import ast
 import json
+from bs4 import BeautifulSoup
 from nsepy.liveurls import quote_eq_url, quote_derivative_url, option_chain_url
 
+OPTIONS_CHAIN_SCHEMA = [str, int, int, int, float, float, float, int, float, float, int,
+                        float,
+                        int, float, float, int, float, float, float, int, int, int, str]
+OPTIONS_CHAIN_HEADERS = ["Call Chart", "Call OI", "Call Chng in OI", "Call Volume", "Call IV", "Call LTP", "Call Net Chng", "Call Bid Qty", "Call Bid Price", "Call Ask Price", "Call Ask Qty",
+                         "Strike Price",
+                         "Put Bid Qty", "Put Bid Price", "Put Ask Price", "Put Ask Qty", "Put Net Chng", "Put LTP", "Put IV", "Put Volume", "Put Chng in OI", "Put OI", "Put Chart"]
+OPTIONS_CHAIN_INDEX = "Strike Price"
 
 eq_quote_referer = "https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol={}&illiquid=0&smeFlag=0&itpFlag=0"
 derivative_quote_referer = "https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuoteFO.jsp?underlying={}&instrument={}&expiry={}&type={}&strike={}"
 option_chain_referer = "https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=-9999&symbol=NIFTY&symbol=BANKNIFTY&instrument=OPTIDX&date=-&segmentLink=17&segmentLink=17"
+
 
 def get_quote(symbol, series='EQ', instrument=None, expiry=None, option_type=None, strike=None):
     """
@@ -24,15 +33,19 @@ def get_quote(symbol, series='EQ', instrument=None, expiry=None, option_type=Non
     """
 
     if instrument:
-        expiry_str = "%02d%s%d"%(expiry.day, months[expiry.month][0:3].upper(), expiry.year)
-        quote_derivative_url.session.headers.update({'Referer': eq_quote_referer.format(symbol)})
-        strike_str = "{:.2f}".format(strike) if strike else "" 
-        res = quote_derivative_url(symbol, instrument, expiry_str, option_type, strike_str)
+        expiry_str = "%02d%s%d" % (
+            expiry.day, months[expiry.month][0:3].upper(), expiry.year)
+        quote_derivative_url.session.headers.update(
+            {'Referer': eq_quote_referer.format(symbol)})
+        strike_str = "{:.2f}".format(strike) if strike else ""
+        res = quote_derivative_url(
+            symbol, instrument, expiry_str, option_type, strike_str)
     else:
-        quote_eq_url.session.headers.update({'Referer': eq_quote_referer.format(symbol)})
+        quote_eq_url.session.headers.update(
+            {'Referer': eq_quote_referer.format(symbol)})
         res = quote_eq_url(symbol, series)
 
-    d =  json.loads(res.text)['data'][0]
+    d = json.loads(res.text)['data'][0]
     res = {}
     for k in d.keys():
         v = d[k]
@@ -47,12 +60,25 @@ def get_quote(symbol, series='EQ', instrument=None, expiry=None, option_type=Non
         res[k] = v_
     return res
 
+
 def get_option_chain(symbol, instrument=None, expiry=None):
 
     if expiry:
-        expiry_str = "%02d%s%d"%(expiry.day, months[expiry.month][0:3].upper(), expiry.year)
+        expiry_str = "%02d%s%d" % (
+            expiry.day, months[expiry.month][0:3].upper(), expiry.year)
     else:
         expiry_str = "-"
     option_chain_url.session.headers.update({'Referer': option_chain_referer})
     r = option_chain_url(symbol, instrument, expiry_str)
 
+    return r
+
+
+def get_option_chain_table(symbol, instrument=None, expiry=None):
+    optchainscrape = get_option_chain(symbol, instrument, expiry)
+    html_soup = BeautifulSoup(optchainscrape.text, 'html.parser')
+    sptable = html_soup.find("table", {"id": "octable"})
+    tp = ParseTables(soup=sptable,
+                     schema=OPTIONS_CHAIN_SCHEMA,
+                     headers=OPTIONS_CHAIN_HEADERS, index=OPTIONS_CHAIN_INDEX)
+    return tp.get_df()
