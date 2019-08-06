9a4e19d70af1ad577f0aa190ffe00abbd7a39592
Santhosh Reddy D <dsanr1@gmail.com>
2019-08-06 18:32:26 +0530

    added get_total_returns_index_history to init

--- nsepy/__init__.py
+++ nsepy/__init__.py
@@ -1,4 +1,4 @@
-from .history import get_history, get_index_pe_history, get_rbi_ref_history
+from .history import get_history, get_index_pe_history, get_rbi_ref_history, get_total_returns_index_history
 from .live import get_quote
 from .derivatives import get_expiry_date
 
